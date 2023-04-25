ffb2810bf49c900f210d8c9fe280d41dffdb0e0b
alvin0319 <alvin7810@naver.com>
2023-04-25 22:40:11 +0900

    LICENSE

--- src/main/kotlin/dev/minjae/votifier/server/server/AuthHandler.kt
+++ src/main/kotlin/dev/minjae/votifier/server/server/AuthHandler.kt
@@ -1,3 +1,14 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright 2023 alvin0319
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package dev.minjae.votifier.server.server
 
 import com.fasterxml.jackson.module.kotlin.readValue
@@ -27,7 +38,6 @@ class AuthHandler(val server: TCPServer, val channel: SocketChannel) : ChannelIn
                 return
             }
             client.setAuthenticated()
-            channel.writeAndFlush("AABBCCDDEE")
             client.sendPacket(MessageData(MessageData.OpCode.LOGIN_SUCCESS.code, "Authentication successful"))
             ctx.pipeline().remove(NAME)
             ctx.pipeline().addLast(MessageHandler.NAME, MessageHandler(server))
